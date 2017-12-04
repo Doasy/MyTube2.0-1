f23f590d36054f905f3bcc7555e86f2a40aaca9e
Alba Lamas <alba.v.lmas@gmail.com>
2017-12-04 21:30:36 +0100

    New class "server" with its attributes

---
+++ src/main/java/BO/Server.java
@@ -0,0 +1,31 @@
+package BO;
+
+public class Server {
+    int id;
+    int port;
+    String host;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
