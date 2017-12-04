162a6acb53159775113c786ad817c72aeb3ae8f0
Alba Lamas <alba.v.lmas@gmail.com>
2017-12-04 21:11:39 +0100

    Define user API methods

--- src/main/java/RestWeb/API.java
+++ src/main/java/RestWeb/API.java
@@ -2,11 +2,9 @@ package RestWeb;
 
 import RestBean.BeanJson;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -65,7 +63,21 @@ public class API {
     public String downloadContent(@PathParam("contentID") String contentID){
         return null;
     }
+    @Path("/content/{contentID}")
+    @PUT
+    public Response updateContent(@FormParam("userID") String userID, @Context HttpServletResponse servletResponse){
+        /*EXEMPLE ----> https://www.tutorialspoint.com/restful/restful_methods.htm */
+        return null;
+    }
+
+    @Path("/content/{contentID}")
+    @DELETE
+    public Response deleteContent(@PathParam("contentID") String userID){
+        /*EXEMPLE ----> https://www.tutorialspoint.com/restful/restful_methods.htm */
+        return null;
+    }
     /*-------USER-------*/
+
     @Path("/user/sign")
     @POST
     public Response signUser(BeanJson bean){
@@ -73,12 +85,30 @@ public class API {
         return Response.status(201).entity(resultat).build();*/
         return null;
     }
+
     @Path("/user/{userID}/content")
     @GET
     //Si volem XML tmb es pot
     @Produces(MediaType.APPLICATION_JSON)
-    public String contentFromUser(@PathParam("contentID") String contentID){
+    public String contentFromUser(@PathParam("userID") String userID){
         return null;
     }
+
+    @Path("/user/login")
+    @POST
+    public Response logUser(BeanJson bean){
+        /*String resultat = bean.getNom();
+        return Response.status(201).entity(resultat).build();*/
+        return null;
+    }
+
+    @Path("/user/{userID}")
+    @GET
+    //Si volem XML tmb es pot
+    @Produces(MediaType.APPLICATION_JSON)
+    public String showUserInformation(@PathParam("userID") String userID){
+        return null;
+    }
+
 
 }
