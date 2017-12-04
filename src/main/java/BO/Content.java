d52a83c460d83aad2731c634851dc6dd3d0fefa9
Alba Lamas <alba.v.lmas@gmail.com>
2017-12-04 21:33:47 +0100

    New class "content" with its attributes

---
+++ src/main/java/BO/Content.java
@@ -0,0 +1,49 @@
+package BO;
+
+public class Content {
+    int id;
+    String title;
+    String description;
+    String uploader;
+    int serverId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUploader() {
+        return uploader;
+    }
+
+    public void setUploader(String uploader) {
+        this.uploader = uploader;
+    }
+
+    public int getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(int serverId) {
+        this.serverId = serverId;
+    }
+}
