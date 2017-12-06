57a55eceba4e65d3f460c7747247e7f4b98576ea
Alba Lamas <alba.v.lmas@gmail.com>
2017-12-06 23:26:51 +0100

    insert New User methods

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,4 +1,12 @@
 package DAO;
 
+import WebService.JDBC.PostgreSQLJDBC;
+
 public class UserDAO {
+
+    private static PostgreSQLJDBC postrgres = new PostgreSQLJDBC();
+
+    public void insertNewUSer(String userName, String password){
+        postrgres.insertNewUser(userName, password);
+    }
 }
--- src/main/java/WebService/JDBC/PostgreSQLJDBC.java
+++ src/main/java/WebService/JDBC/PostgreSQLJDBC.java
@@ -3,11 +3,13 @@ package WebService.JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PostgreSQLJDBC {
-    DriverManager c;
+    Connection c = null;
+
     public void openConnection() {
-        Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
@@ -15,9 +17,31 @@ public class PostgreSQLJDBC {
                             "postgres", "1234");
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.err.println("problem opening the connection");
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+    }
+
+    public void closeConnection(){
+        try {
+            c.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("problem closing the connection");
+        }
+        System.out.println("Closed database successfully");
+    }
+
+    public void insertNewUser(String userName, String password){
+        Statement stmt;
+        try {
+            stmt = c.createStatement();
+            String sql = "INSERT INTO user (username, password) "
+                    + "VALUES ('"+userName+"', '"+password+"');";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.err.println("problem executing the query");
+        }
     }
 }
