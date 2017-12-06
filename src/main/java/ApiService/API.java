7981e5d1e8f63c0570dc26ef1435f609f16146ab
Alba Lamas <alba.v.lmas@gmail.com>
2017-12-06 14:05:28 +0100

    refactor API

--- src/main/java/ApiService/API.java
+++ src/main/java/ApiService/API.java
@@ -15,69 +15,10 @@ import javax.ws.rs.core.Response;
 @Path("")
 public class API {
     /*-------SERVER-------*/
-    @Path("/server/{serverID}")
-    @GET
-    //Si volem XML tmb es pot
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getServerInfo(@PathParam("serverID") String serverID){
-        //Show the information of the server with ID serverId
-        return null;
-    }
 
-    @Path("/server/{serverID}/{content}")
-    @GET
-    //Si volem XML tmb es pot
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getAllContent(@PathParam("serverID") String serverID, @PathParam("content") String content){
-
-        return null;
-    }
 
     /*-------CONTENT-------*/
-    @Path("/content")
-    @GET
-    //Si volem XML tmb es pot
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getSearchContent(){
-        return null;
-    }
 
-    @Path("/content")
-    @POST
-    public Response uploadContent(BeanJson bean){
-        /*String resultat = bean.getNom();
-        return Response.status(201).entity(resultat).build();*/
-        return null;
-    }
-
-    @Path("/content/{contentID}")
-    @GET
-    //Si volem XML tmb es pot
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getSearchContentByID(@PathParam("contentID") String contentID){
-        return null;
-    }
-
-    @Path("/download/{contentID}")
-    @GET
-    //Si volem XML tmb es pot
-    @Produces(MediaType.APPLICATION_JSON)
-    public String downloadContent(@PathParam("contentID") String contentID){
-        return null;
-    }
-    @Path("/content/{contentID}")
-    @PUT
-    public Response updateContent(@FormParam("userID") String userID, @Context HttpServletResponse servletResponse){
-        /*EXEMPLE ----> https://www.tutorialspoint.com/restful/restful_methods.htm */
-        return null;
-    }
-
-    @Path("/content/{contentID}")
-    @DELETE
-    public Response deleteContent(@PathParam("contentID") String userID){
-        /*EXEMPLE ----> https://www.tutorialspoint.com/restful/restful_methods.htm */
-        return null;
-    }
     /*-------USER-------*/
 
     @Path("/user/sign")
